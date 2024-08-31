ae524248f19baa63149039fd7ef431ad304b743d
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2024-08-31 11:20:16 +0300

    create project with UA

---
+++ AirportDatabase/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using _03_EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AirportDatabase
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+     
+        AirplaneDbContex contex = new AirplaneDbContex();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           dataGrid.ItemsSource =   contex.Flights.ToList();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            dataGrid.ItemsSource = contex.Clients.ToList();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            dataGrid.ItemsSource = contex.Airplanes.ToList();
+        }
+    }
+}
